69f042d6d4ed60f8f370801406ac4676c037c378
Ibtsam Ahmad <ibtsamahmad01@gmail.com>
2023-06-25 10:58:38 +0500

    suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from "react";
 import { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
